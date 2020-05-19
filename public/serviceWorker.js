d4470c050084137dca9aaf1839df98cfae2674c1
Samantha Scott <Samanthascott1992@gmail.com>
2020-05-19 12:59:09 -0600

    public folder

---
+++ public/serviceWorker.js
@@ -0,0 +1,98 @@
+import { response } from "express";
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_to_CACHE = [
+
+    "/",
+    "/indexedDB.js",
+    "/index.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_to_CACHE);
+        })
+    );
+
+ });
+
+// self.addEventListener('activate', event => {
+//     event.waitUntil(
+//         cache.keys().then(keyList => {
+
+//             return Promise.all(
+//                 keyList.map(key => {
+
+//                 if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+
+//                     return caches.delete(key);
+//                 };
+
+//                 })
+//             );
+//         })
+//     );
+
+//     self.clients.claim();
+// });
+
+self.addEventListener("fetch", event => {
+
+    if(event.request.url.includes("/api/")) {
+
+        event.respondWith(
+
+        caches.open(DATA_CACHE_NAME).then(cache => {
+
+            return fetch (event.request)
+
+            .then(response => {
+
+                if (response.status === 200) {
+                    cache.put(event.request.url, response.clone());
+                }
+                return response;
+            })
+            .catch(err => {
+                return cache.match(event.request);
+            });
+
+        }).catch(err => {
+
+            console.log(err);
+
+        })
+
+        );
+
+        return;
+    }
+
+    event.respondWith(
+
+        fetch(event.request).catch(() => {
+
+            return caches.match(event.request).then(response => {
+
+                if (response) {
+
+                    return response
+
+                } else if (event.request.headers.get("accept").include("text/html")) {
+
+                    return caches.match("/")
+                }
+            })
+            
+        })
+    )
+
+})
