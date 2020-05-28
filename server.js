1691d72fa194e1601b9cb3616964c9a626f629e4
Samantha Scott <Samanthascott1992@gmail.com>
2020-05-28 10:30:31 -0600

    working working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,13 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI);
-
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
